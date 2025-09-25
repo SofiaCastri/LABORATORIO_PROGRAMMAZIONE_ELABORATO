3f6a0b79d0abf21c150971df9d9690013d70275c
sofiacastri <sofia.castri@edu.unifi.it>
2025-09-25 12:40:54 +0200

    aggiunto classe Account con costruttore, getter, metodi base

---
+++ Account.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Sofia Castri on 25/09/25.
+//
+
+#include "Account.h"
+
+Account::Account(const string &name, const string &iban, double balance) : name(name), iban(iban), balance(balance){}
+
+// Getter
+std::string Account::getName() const {
+    return name;
+}
+
+std::string Account::getIban() const {
+    return iban;
+}
+
+double Account::getBalance() const {
+    return balance;
+}
+
+
+void Account::addTransaction(const Transaction& t) {
+    transactions.push_back(t);
+
+    if (t.getType() == TransactionType::Incoming)
+        balance += t.getAmount();
+    else
+        balance -= t.getAmount();
+}
+
+
+// Trasferimento tra conti
+void Account::transferTo(double amount, Account& receiver, const std::string& description) {
+    if (amount <= 0)
+        throw std::invalid_argument("Importo deve essere positivo");
+    if (balance < amount)
+        throw std::runtime_error("Saldo insufficiente per trasferimento");
+
+    int transactionId = rand() % 90000 + 10000; // ID casuale per la transazione
+
+    Transaction senderT(transactionId, amount, TransactionType::Outgoing, description);
+    Transaction receiverT(transactionId, amount, TransactionType::Incoming, description);
+
+    addTransaction(senderT);        // aggiorna saldo mittente
+    receiver.addTransaction(receiverT); // aggiorna saldo destinatario
+}
+
+
+vector<Transaction> Account::searchTransactionByType(TransactionType type) const {
+    vector<Transaction> filteredTransactions;
+    for (const auto& transaction : transactions) {
+        if (transaction.getType() == type) {
+            filteredTransactions.push_back(transaction);;
+        }
+    }
+    return filteredTransactions;
+}
---
+++ Account.h
@@ -0,0 +1,38 @@
+//
+// Created by Sofia Castri on 25/09/25.
+//
+
+#ifndef LABORATORIO_PROGRAMMAZIONE_ELABORATO_ACCOUNT_H
+#define LABORATORIO_PROGRAMMAZIONE_ELABORATO_ACCOUNT_H
+
+#include "Transaction.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Account {
+public: Account(const std::string& name, const std::string& iban, double balance);
+
+    // Getter
+    std::string getName() const;
+    std::string getIban() const;
+    double getBalance() const;
+
+
+    //metodi principali
+    void addTransaction(const Transaction& t); // aggiunge una transazione
+    void transferTo(double amount, Account& receiver, const std::string& description); // trasferisce fondi a un altro conto
+    std::vector<Transaction> searchTransactionByType(TransactionType type) const;
+
+
+private:
+    string name; // Nome del titolare
+    string iban; // IBAN del conto
+    double balance; // Saldo corrente
+    vector<Transaction> transactions; // Lista delle transazioni
+
+};
+
+
+#endif //LABORATORIO_PROGRAMMAZIONE_ELABORATO_ACCOUNT_H
